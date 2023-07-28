1b5c83b00ac90fb5f9c3101ee5756f896c6f62e1
pedro <pedromartinf07@gmail.com>
2023-07-27 23:47:40 -0600

    token PATH and file NAME

--- src/CLI.py
+++ src/CLI.py
@@ -12,14 +12,14 @@ tokens = [
     'ID',
     'STRING',
     'NUMBER',
+    'PATH',
+    'FILENAME',
     'EQUALTO', # =
     'DASH',  # -
-    'DOT',   # .
 ]+list(reserve.values())
 
 t_EQUALTO =         r'='
 t_DASH =            r'-'
-t_DOT =             r'.'
 
 def t_DECIMALNUM(t):
     r'\d+\.\d+'
@@ -39,6 +39,20 @@ def t_INTNUM(t):
         t.value = 0
     return t
 
+def t_PATH(t):
+    r'\/[a-zA-Z0-9_\/]*\/'
+    return t
+
+def t_FILENAME(t):
+    r'[a-zA-Z0-9_]+\.bin' 
+    return t
+
+# def t_STRING(t):
+#     r'"[^"]*"'
+#     t.value = t.value[1:-1]
+#     return t
+
+
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z_0-9]*'
     t.type = reserve.get(t.value.lower(), 'ID')    # Check for reserved words
@@ -55,14 +69,14 @@ def t_newline(t):
 def t_error(t):
     # error.append(Exception('Lexico', 'Error Lexico: ' +
     #              t.value[0], t.lineno, find_column(input, t)))
-    print("Error Lexico {t.value[0]} {t.lineno} ")
+    print('Error Lexico: '+t.value[0]+' line: '+str(t.lineno)+' column: '+str(find_column(input, t)))
     t.lexer.skip(1)
 
 def find_column(input, token):
     line_start = input.rfind('\n', 0, token.lexpos)+1
     return (token.lexpos - line_start) + 1
 
-#sintax grammar
+#sintax grammar-----------------------------------------------------------------------
 def p_init(t):
     '''init :   instructions'''
     t[0] = t[1]
@@ -92,8 +106,8 @@ def p_instruction(t):
     t[0] = t[1]
 
 def p_exec_instruction(t):
-    '''exec_instruction :   EXEC ID'''
-    print(t[2])
+    '''exec_instruction :   EXEC PATH FILENAME'''
+    print(t[2]+" "+t[3])
 
 
 def p_mkdisk_instruction(t):
@@ -106,10 +120,10 @@ def p_fdisk_instruction(t):
     print(t[2])
     t[0] = t[1]
 
+input = ''
 def cli_command(command):
     # construct to lexer analyzer
+    input = command
     lexer = lex.lex()
-
     parser = yacc.yacc()
-
     parser.parse(command)
