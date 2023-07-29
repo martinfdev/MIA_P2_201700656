5d105b83bb49dda4e66380aa78192e03dd7f5fed
pedro <pedromartinf07@gmail.com>
2023-07-28 23:48:46 -0600

    get random number between

---
+++ src/Functions.py
@@ -0,0 +1,9 @@
+import random
+import math
+class Functions:
+    def __init__(self):
+        pass
+
+    def random_mumber(self, init, end):
+        return math.floor(random.uniform(init, end))
+        
