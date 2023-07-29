f8e10f1f58e9c9221df0a6350edfed5e2f8892a3
pedro <pedromartinf07@gmail.com>
2023-07-28 23:49:55 -0600

    get date and time format time stamp

--- src/Functions.py
+++ src/Functions.py
@@ -1,9 +1,14 @@
 import random
 import math
+import datetime
 class Functions:
     def __init__(self):
         pass
 
     def random_mumber(self, init, end):
         return math.floor(random.uniform(init, end))
-        
+
+    def time_stamp_obj(self):
+        time_s = datetime.datetime.now()
+        print(time_s.day)
+        return time_s    
