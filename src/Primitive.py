bdc108aacf85ddee44644b58b34cf9ce690ed345
pedro <pedromartinf07@gmail.com>
2023-08-05 13:36:00 -0600

    class Size, Path, Fit and Unit

---
+++ src/Primitive.py
@@ -0,0 +1,36 @@
+class Size:
+    def __init__(self, value=None):
+        self.value = value
+
+    def get_value(self):
+        if self.value == None:
+            return 0
+        return int(self.value)
+
+class Path:
+    def __init__(self, path, filename):
+        self.path = path
+        self.filename = filename
+        
+    def __str__(self):
+        if self.path == None:
+            return ""
+        return self.path + self.filename
+    
+class Fit:
+    def __init__(self, value=None):
+        self.value = value
+
+    def get_value(self):
+        if self.value == None:
+            return "FF"
+        return str.upper(self.value) #return value in uppercase
+
+class Unit:
+    def __init__(self, value=None):
+        self.value = value
+
+    def get_value(self):
+        if self.value == None:
+            return "M"
+        return str.upper(self.value) #return value in uppercase    
