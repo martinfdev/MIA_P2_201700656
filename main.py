d742dab05f4bada9229304ef225dcc528235dd1c
pedro <pedromartinf07@gmail.com>
2023-08-04 23:42:39 -0600

    input with loop form

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from src.BinaryFileManager import BinaryFileManager
+from src.CLI import cli_command
+from src.Exec import Exec
+import src.Functions as fn
 
 if __name__ == "__main__":
-    file_manager = BinaryFileManager("archivo.bin")
-    data_binary = b'\0'
-    buffer = data_binary * 1024
-    status = file_manager.exist_file()
-    # file_manager.write_binary_data(buffer)
+    while True:
+        command = input(">> ")
+        if command == "exit":
+            break
+        content = cli_command(command)
+        if content != "":
+            print(cli_command(content))
