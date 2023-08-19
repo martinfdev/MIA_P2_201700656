5c3cee435d9921d383629bcbce7761a63756a16e
pedro <pedromartinf07@gmail.com>
2023-08-19 12:02:19 -0600

    fix sirialize and desarileze data mbr and partition

--- src/UtilClass.py
+++ src/UtilClass.py
@@ -6,24 +6,25 @@ class MBR:
         self.mbr_tamano = 0 # 4 bytes tipo int
         self.mbr_time_stamp = 0 # 4 bytes tipo int
         self.mbr_disk_signature = 0 # 4 bytes tipo int
-        self.mbr_disk_fit = b'\0' # 1 byte tipo char
+        self.mbr_disk_fit = '0' # 1 byte tipo char
         self.mbr_partition_1 = Partition()
         self.mbr_partition_2 = Partition()
         self.mbr_partition_3 = Partition()
         self.mbr_partition_4 = Partition()
     
-    def setValues(self, mbr_tamano, mbr_time_stamp, disk_signature, mbr_disk_fit):
+    def setValues(self, mbr_tamano, disk_signature, mbr_disk_fit):
         self.mbr_tamano = mbr_tamano
-        self.mbr_time_stamp = mbr_time_stamp
+        self.mbr_time_stamp = fn().get_time_stamp_now()
         self.mbr_disk_signature = disk_signature
-        self.mbr_disk_fit = fn().string_to_bytes(mbr_disk_fit)
+        self.mbr_disk_fit = mbr_disk_fit
         self.mbr_partition_1 = Partition()
         self.mbr_partition_2 = Partition()
         self.mbr_partition_3 = Partition()
         self.mbr_partition_4 = Partition()
 
     def serialize_mbr(self):
-        bytesMBR = fn().serialize(self.FORMATMBR, self.mbr_tamano, self.mbr_time_stamp, self.mbr_disk_signature, self.mbr_disk_fit)
+        fns = fn()
+        bytesMBR = fns.serialize(self.FORMATMBR, self.mbr_tamano,  fns.get_time_stamp(self.mbr_time_stamp), self.mbr_disk_signature, fns.string_to_bytes(self.mbr_disk_fit))
         return bytesMBR + self.mbr_partition_1.serialize_partition() + self.mbr_partition_2.serialize_partition() + self.mbr_partition_3.serialize_partition() + self.mbr_partition_4.serialize_partition()
     
     def deserialize_mbr(self, data):
@@ -57,36 +58,48 @@ class MBR:
 class Partition:
     def __init__(self) -> None:
         self.FORMATPARTITION = '3c2I16s' # 1 char, 1 char, 2 int unsigned, 16 char
-        self.part_status = b'\0' # 1 byte tipo char
-        self.part_type = b'\0' # 1 byte tipo char
-        self.part_fit = b'\0' # 1 byte tipo char
+        self.part_status = '\0' # 1 byte tipo char
+        self.part_type = '\0' # 1 byte tipo char
+        self.part_fit = '\0' # 1 byte tipo char
         self.part_start = 0 # 4 bytes tipo int
         self.part_size = 0 # 4 bytes tipo int
-        self.part_name = b'\0'* 16 # 16 bytes char[16]    
+        self.part_name = '\0'* 16 # 16 bytes char[16]    
 
     def setValues(self, part_status, part_type, part_fit, part_start, part_size, part_name):
         fns = fn()
-        self.part_status = fns.string_to_bytes(part_status)
-        self.part_type = fns.string_to_bytes(part_type)
-        self.part_fit = fns.string_to_bytes(part_fit)
+        self.part_status = part_status
+        self.part_type = part_type
+        self.part_fit = part_fit
         self.part_start = part_start
         self.part_size = part_size
-        self.part_name = fns.string_to_bytes(part_name)
+        self.part_name = part_name
 
     def serialize_partition(self):
-        return fn().serialize(self.FORMATPARTITION, self.part_status, self.part_type, self.part_fit, self.part_start, self.part_size, self.part_name)
+        fns = fn()
+        return fn().serialize(self.FORMATPARTITION, fns.string_to_bytes(self.part_status), fns.string_to_bytes(self.part_type), fns.string_to_bytes(self.part_fit), self.part_start, self.part_size, fns.string_to_bytes(self.part_name))
 
     def deserialize_partition(self, data, num_partition = 1):
-        partiontion_data = fn().deserialize(self.FORMATPARTITION, data)
+        fns = fn()
+        partiontion_data = fns.deserialize(self.FORMATPARTITION, data)
         if partiontion_data is None:
             fn().err_msg("PARTITION", "No se pudo deserializar la partición" + str(num_partition))
             return self
-        self.part_status = partiontion_data[0]
-        self.part_type = partiontion_data[1]
-        self.part_fit = partiontion_data[2]
+        
+        if partiontion_data[0] == b'\x00' and partiontion_data[1] == b'\x00' and partiontion_data[2] == b'\x00' and partiontion_data[3] == 0 and partiontion_data[4] == 0:
+            self.part_status = '\0'
+            self.part_type = '\0'
+            self.part_fit = '\0'
+            self.part_start = 0
+            self.part_size = 0
+            self.part_name = '\0'* 16
+            return self
+
+        self.part_status = fns.bytes_to_string(partiontion_data[0])
+        self.part_type = fns.bytes_to_string(partiontion_data[1])
+        self.part_fit = fns.bytes_to_string(partiontion_data[2])
         self.part_start = partiontion_data[3]
         self.part_size = partiontion_data[4]
-        self.part_name = partiontion_data[5]
+        self.part_name = fns.bytes_to_string(partiontion_data[5])
         return self
     
 #-------------------EBR-------------------
