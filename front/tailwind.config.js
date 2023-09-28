3ea4ecfc7d1e667e35def6adb5876a5e3aa1fec5
pedro <pedromartinf07@gmail.com>
2023-09-28 11:34:10 -0600

    config tailwindcsss add files

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FF6363',
+        'secondary': {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+        'warning': '#FFA500',
+        'danger': '#FF0000',
+        'success': '#008000',
+        'info': '#00FFFF',
+        'dark': '#000000',
+        'light': '#FFFFFF'
+      },
+    },
   },
   plugins: [],
 }
-
