8b995bee750ad2e16a094ade0aca4fa75afc2c69
pedro <pedromartinf07@gmail.com>
2023-10-02 23:02:39 -0600

    login component

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -3,6 +3,9 @@ function Login(){
     return(
         <div>
             <h1>Login</h1>
+            <input type="label" />
+            <input type="password" />
+            <button>Log in</button>
         </div>
     )
 }
