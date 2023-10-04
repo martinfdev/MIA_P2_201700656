2440f0e98ad983f43fcc3d924d147aa7402d07a0
pedro <pedromartinf07@gmail.com>
2023-10-03 19:12:24 -0600

    hover change

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import FileInput from '../components/FileInput';
 import TextArea from '../components/TextArea';
 import Header from '../components/Header';
@@ -25,7 +24,7 @@ function Home() {
                 <input name="filename" type="text" defaultValue={fileName} className="w-4/5 h-10 hover:border-danger rounded-sm px-4 py-2" />
                 <FileInput onFileContent={setFileContent} onFileName={setFileName} />
                 <button
-                    className="w-1/4 h-10 rounded-sm bg-slate-500 text-white font-bold border-2 hover:border-primary"
+                    className="w-1/4 h-10 rounded-sm bg-slate-500 text-white font-bold border-2 hover:border-danger"
                     onClick={handleOutputChange}
                 >Ejecutar</button>
             </div>
