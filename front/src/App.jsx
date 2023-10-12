5896332f9dcc18547fe4ceeb451b39b68c77b383
pedro <pedromartinf07@gmail.com>
2023-10-11 22:59:03 -0600

    del hiden div

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,20 +3,20 @@ import { Route, Routes } from 'react-router-dom'
 //pages
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Reports from './pages/Reports'
 import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <>
-      <div className="bg-dark">
+      <div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/*" element={<NotFound />} />
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/reports" element={<Reports />} />
           </Routes>
       </div>
-    </>
   )
 }
 export default App
