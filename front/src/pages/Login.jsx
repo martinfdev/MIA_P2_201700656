2efc896ada70d926c4857bb5f5daea9b8cdb981d
pedro <pedromartinf07@gmail.com>
2023-10-24 22:45:34 -0600

    redirect to reports

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,6 +1,14 @@
-import {AiOutlineEye} from 'react-icons/ai';
+import { useNavigate } from "react-router-dom"
+// import {AiOutlineEye} from 'react-icons/ai'
+
 
 function Login() {
+    const navigate = useNavigate();
+    
+    const handleReports = () => {
+        navigate('/reports')
+    }
+
     return (
         <div className="flex h-screen justify-center items-center">
             <div className="flex flex-col items-center justify-center gap-14 rounded-md bg-slate-600 w-auto h-auto p-10">
@@ -18,9 +26,11 @@ function Login() {
                         type="password"
                         placeholder="Password"
                         className="p-2 rounded-md bg-slate-200 w-full hover:bg-blue-200 border-2 hover:border-primary hover:shadow-md" />
-                    <AiOutlineEye className="text-2xl text-slate-200" />
+                    {/* <AiOutlineEye className="text-2xl text-slate-200" /> */}
                 </div>
-                <button className="p-3 rounded-md text-slate-900 font-bold bg-slate-200 border-4 hover:border-primary  w-full">Ingresar</button>
+                <button className="p-3 rounded-md text-slate-900 font-bold bg-slate-200 border-4 hover:border-primary  w-full"
+                onClick={handleReports}
+                >Ingresar</button>
             </div>
         </div>
     )
