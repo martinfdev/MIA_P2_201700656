f5efe0ef9e0090cb88915fd09ef3378b6c381a96
pedro <pedromartinf07@gmail.com>
2023-10-24 22:44:44 -0600

    post and response value

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -14,19 +14,44 @@ function Home() {
     }
 
     const handleOutputChange = () => {
-        setOutput(fileContent)
+        fetch('http://localhost:5000/execute', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "content": fileContent
+            })
+        }).then(res => res.json())
+            .then(data => {
+                var output_console = data.output
+                var string_output_console = ""
+                for (let i = 0; i < output_console.length; i++) {
+                    string_output_console += output_console[i] + "\n"
+                }
+                setOutput(string_output_console)
+            }).catch(err => console.log(err))
     }
 
+    const handleCleanTextArea = () => {
+        setFileContent("")
+        setOutput("Salida>>")
+    }
+
+
     return (
-        <div className="flex flex-col h-screen w-screen gap-5 p-2 ">
+        <div className="flex flex-col h-screen w-screen gap-3 p-2 ">
             <Header />
-            <div className="flex flex-row gap-3">
+            <div className="flex flex-row gap-8">
                 <input name="filename" type="text" defaultValue={fileName} className="w-4/5 h-10 hover:border-danger rounded-sm px-4 py-2" />
                 <FileInput onFileContent={setFileContent} onFileName={setFileName} />
                 <button
                     className="w-1/4 h-10 rounded-sm bg-slate-500 text-white font-bold border-2 hover:border-danger"
                     onClick={handleOutputChange}
                 >Ejecutar</button>
+                <button className="w-1/4 h-10 rounded-sm bg-slate-500 text-white font-bold border-2 hover:border-danger"
+                    onClick={handleCleanTextArea}
+                >Limpiar</button>
             </div>
             <TextArea value={fileContent} onChange={handleTextChange} isEditable={true} />
             <TextArea value={output} onChange={handleTextChange} isEditable={false} />
